7dd72fc653a34c688b674a9337aaf77285e21def
Xiong Zhang <xiong.zhang@wesion.com>
2023-10-26 15:10:52 +0800

    LCD: add lcd reset control
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- common/cmd_kbi.c
+++ common/cmd_kbi.c
@@ -721,6 +721,17 @@ static int do_kbi_version(cmd_tbl_t * cmdtp, int flag, int argc, char * const ar
 
 }
 
+#if defined(CONFIG_KHADAS_VIM3) || defined(CONFIG_KHADAS_VIM3L)
+extern int tca6408_output_set_value(u8 value, u8 mask);
+static int do_kbi_lcd_reset(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
+{
+	tca6408_output_set_value(0<<0, 1<<0);
+	udelay(100);
+	tca6408_output_set_value(1<<0, 1<<0);
+	return 0;
+}
+#endif
+
 static int do_kbi_usid(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 {
 	if (argc == 2) {
@@ -1067,6 +1078,7 @@ static cmd_tbl_t cmd_kbi_sub[] = {
 	U_BOOT_CMD_MKENT(bootmode, 3, 1, do_kbi_bootmode, "", ""),
 #if defined(CONFIG_KHADAS_VIM3) || defined(CONFIG_KHADAS_VIM3L)
 	U_BOOT_CMD_MKENT(portmode, 1, 1, do_kbi_portmode, "", ""),
+	U_BOOT_CMD_MKENT(lcd_reset, 1, 1, do_kbi_lcd_reset, "", ""),
 #endif
 	U_BOOT_CMD_MKENT(forcereset, 4, 1, do_kbi_forcereset, "", ""),
 	U_BOOT_CMD_MKENT(factorytest, 1, 1, do_kbi_factorytest, "", ""),
