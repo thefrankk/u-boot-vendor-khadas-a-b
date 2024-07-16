3cdff93c52337753822c560188b4debbacc22077
goenjoy <goenjoy@khadas.com>
2024-07-16 19:56:46 +0800

    Modify dts loading t7_a311d2_vim4/t7c_a311d2_vim4n
    Change-Id: Iebdcf6f93ac4e69fc1a04f15eb91c7b75727d28a
    Signed-off-by: goenjoy <goenjoy@khadas.com>

--- board/khadas/kvim4/kvim4.c
+++ board/khadas/kvim4/kvim4.c
@@ -524,31 +524,31 @@ int checkhw(char * name)
 			#ifdef CONFIG_HDMITX_ONLY
 			strcpy(loc_name, "t7_a311d2_an400-hdmitx-only\0");
 			#else
-			strcpy(loc_name, "t7_a311d2_an400\0");
+			strcpy(loc_name, "t7_a311d2_vim4\0");
 			#endif
 		} else if (cpu_id.chip_rev == 0xC) {
 			#ifdef CONFIG_HDMITX_ONLY
 			strcpy(loc_name, "t7c_a311d2_an400-hdmitx-only-4g\0");
 			#else
-			strcpy(loc_name, "t7c_a311d2_an400-4g\0");
+			strcpy(loc_name, "t7c_a311d2_vim4n\0");
 			#endif
 		}
 		break;
 	case CONFIG_T7_8G_SIZE:
 		if (cpu_id.chip_rev == 0xA || cpu_id.chip_rev == 0xb) {
-			strcpy(loc_name, "t7_a311d2_an400-8g\0");
+			strcpy(loc_name, "t7_a311d2_vim4\0");
 		} else if (cpu_id.chip_rev == 0xC) {
-			strcpy(loc_name, "t7c_a311d2_an400-8g\0");
+			strcpy(loc_name, "t7c_a311d2_vim4n\0");
 			//
 		}
 		break;
 	default:
 		printf("DDR size: 0x%llx, multi-dt doesn't support, ", ddr_size);
-		printf("set default t7_a311d2_an400\n");
+		printf("set defalult kvim4/kvim4c\n");
 		if (cpu_id.chip_rev == 0xA || cpu_id.chip_rev == 0xb) {
-			strcpy(loc_name, "t7_a311d2_an400\0");
+			strcpy(loc_name, "t7_a311d2_vim4\0");
 		} else if (cpu_id.chip_rev == 0xC) {
-			strcpy(loc_name, "t7c_a311d2_an400-4g\0");
+			strcpy(loc_name, "t7c_a311d2_vim4n\0");
 			//
 		}
 		break;
@@ -558,7 +558,7 @@ int checkhw(char * name)
 	strcpy(name, loc_name);
 	env_set("aml_dt", loc_name);
 #else
-	env_set("aml_dt", "t7_a311d2_an400\0");
+	env_set("aml_dt", "t7c_a311d2_an400\0");
 #endif
 
 	return 0;
