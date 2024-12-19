12a8b73c10b9db2e1f8aafb69d5ea019a724dd5a
thefrankk <105942735+thefrankk@users.noreply.github.com>
2024-12-19 18:44:39 -0300

    remove cfgload

--- board/khadas/configs/kvim3.h
+++ board/khadas/configs/kvim3.h
@@ -269,7 +269,6 @@
 		"legacyboot="\
 			"kbi resetflag 0;"\
 			"boot_cooling;"\
-			"cfgload;" \
 			"\0"\
 		"storeboot="\
 			"if load mmc 0:1 1020000 s905_autoscript || load mmc 1:1 1020000 s905_autoscript || load mmc 1:5 1020000 /boot/s905_autoscript; then autoscr 1020000; fi;"\
